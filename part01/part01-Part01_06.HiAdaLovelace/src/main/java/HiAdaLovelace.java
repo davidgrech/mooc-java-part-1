d5eeadcf5187bb6caea3726460216d0ba683f024
David <david.grech26@gmail.com>
2020-11-10 12:10:16 +0000

    Add exercise 5

---
+++ part01/part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,12 @@
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+
+        String greeting = "Hi " + name + "!";
+        
+        System.out.println(greeting);
+        
+    }
+}
