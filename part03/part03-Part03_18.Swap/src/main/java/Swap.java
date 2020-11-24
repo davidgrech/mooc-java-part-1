c8eb1819a94082a3b350dbb6438dcd1eb4491754
David <david.grech26@gmail.com>
2020-11-24 16:34:34 +0000

    Add exercise 18

---
+++ part03/part03-Part03_18.Swap/src/main/java/Swap.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        // Implement here
+        // asking for the two indices
+        // and then swapping them
+        
+        System.out.println("Give two indices to swap:");
+        
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        
+        int firstConstant = array[first];
+        int secondConstant = array[second];
+        
+        array[second] = firstConstant;
+        array[first] = secondConstant;
+
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
