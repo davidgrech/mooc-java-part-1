b4648016b11e565f0dffbed3e842d81873003529
David <david.grech26@gmail.com>
2021-01-20 09:37:16 +0000

    Add exercise 27

---
+++ part03/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class AVClub {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String username = scanner.nextLine();
+        
+        while(!username.equals("")){
+        
+            String[] splitSentence = username.split(" ");
+
+            
+            for(String word : splitSentence){
+                
+                if(word.contains("av")){
+                    System.out.println(word);
+                }
+                
+            }
+        
+            username = scanner.nextLine();
+            
+        }
+    }
+}
