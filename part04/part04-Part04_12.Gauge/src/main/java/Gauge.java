81d56812d83c68dc91cc25348ac3a99d98383568
David <david.grech26@gmail.com>
2021-02-04 11:44:38 +0000

    Add exercise 12

---
+++ part04/part04-Part04_12.Gauge/src/main/java/Gauge.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class Gauge {
+    
+    private int value;
+    
+    public Gauge(){
+        this.value = 0;
+    }
+    
+    public void increase(){
+        
+        if(this.value != 5){
+            this.value ++;
+        }
+        
+    }
+    
+    public void decrease(){
+        
+        if(this.value != 0){
+            this.value --;
+        }
+        
+    }
+    
+    public int value(){
+        return value;
+    }
+    
+    public boolean full(){
+        
+        if(this.value == 5){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+}
