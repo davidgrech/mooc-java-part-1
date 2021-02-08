b02ae10baf8b8125d44438497dae711d8917203b
David <david.grech26@gmail.com>
2021-02-08 20:56:25 +0000

    Add exercise 15

---
+++ part04/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -0,0 +1,42 @@
+/**
+ *
+ * @author david
+ */
+
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+        
+        System.out.println("Enter numbers:");
+        
+        while(true){
+            
+            int number = scanner.nextInt();
+            
+            if(number == -1){
+                break;
+            }
+            
+            allNumbers.addNumber(number);
+            
+            if(number % 2 == 0){
+                evenNumbers.addNumber(number);
+            } else if (number % 2 == 1){
+                oddNumbers.addNumber(number);
+            }
+            
+        }
+        
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
+        
+    }
+}
