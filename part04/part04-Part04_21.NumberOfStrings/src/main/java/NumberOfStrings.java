eda8741e6d2faf30fc790eb78296604d1fb39a59
David <david.grech26@gmail.com>
2021-02-15 14:37:30 +0000

    Add exercise 21

---
+++ part04/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int totalStrings= 0;
+        
+        while (true) {
+            String line = scanner.nextLine();
+
+            if (line.equals("end")) {
+                break;
+            }
+
+            totalStrings++;
+
+        }
+        
+        System.out.println(totalStrings);
+
+    }
+}
