9a32749170e66ea715bebd91baa43871a2ff540e
David <david.grech26@gmail.com>
2021-03-10 09:02:10 +0000

    Add exercise 4

---
+++ part05/part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class Fitbyte {
+    
+    private final int age;
+    private final int restingHeartRate;
+
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+
+    public double targetHeartRate(double percentageOfMaximum) {
+        double maxHeartRate = 206.3 - (0.711 * this.age);
+        return (maxHeartRate - this.restingHeartRate) * percentageOfMaximum + this.restingHeartRate;
+    }
+    
+}
